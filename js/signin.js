330aa4cf9abe8f4168a362264f1f00f4ebb64e0d
VLBortolotti <vitorlacobortolotti>
2023-11-21 01:08:00 -0300

    Feat: signin screen almost fully functional

---
+++ js/signin.js
@@ -0,0 +1,59 @@
+let email       = document.querySelector('#email')
+let senha       = document.querySelector('#senha')
+let msg         = document.querySelector('#msg')
+let entrarBtn   = document.querySelector('button')
+let eyeBtn      = document.querySelector('.fa-eye')
+
+eyeBtn.addEventListener('click', () => {
+    if (senha.getAttribute('type') == 'password') {
+        senha.setAttribute('type', 'text')
+    } else {
+        senha.setAttribute('type', 'password')
+    }
+})
+
+entrarBtn.addEventListener('click', () => {
+    const userData = {
+        email: email.value,
+        password: senha.value,
+    }
+
+    console.log(userData)
+
+    const apiUrl = 'http://localhost:3000/users/login'
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        if ('erro' in data) {
+            msg.style.color = '#ff0000'
+            msg.style.backgroundColor = '#ffbbbb'
+            msg.style.display = 'block'
+            msg.style.fontWeight = 'bold'
+            msg.innerHTML = data.erro
+            
+            console.log(`Error: ${data.erro}`)
+        } 
+
+        else if (!('erro' in data) && (data.msg === "ok") && (Object.keys(data.data).length > 0)) {
+            msg.style.color = '#272262'
+            msg.style.backgroundColor = '#ffffff'
+            msg.style.display = 'block'
+            msg.style.fontWeight = 'bold'
+            msg.innerHTML = 'Login feito com sucesso'
+
+            console.log(`Data: ${JSON.stringify(data.data, null, 2)}`)
+        }
+    })
+    .catch( (error) => {
+        console.error(`Fetch error: ${error}`)
+    })
+})
