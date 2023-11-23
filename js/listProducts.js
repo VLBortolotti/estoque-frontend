6b2e47a0cc55525785a17da6d9b22a2185525e92
VLBortolotti <vitorlacobortolotti>
2023-11-22 23:18:08 -0300

    Feat: listProducts fully functional

--- js/listProducts.js
+++ js/listProducts.js
@@ -24,4 +24,52 @@ userData     = JSON.parse(localStorage.getItem('userData'))
 let userName = document.querySelector("#userName")
 userName.innerHTML = userData.name
 
-// Pegando dados do form de cadastro de produtos
+// Pegando dados do form dos produtos e populando a tabela (no HTML)
+const apiUrl = 'http://localhost:3000/products' 
+const table  = document.querySelector('#tabela')
+
+fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+.then(response => response.json())
+.then( (data) => {
+  console.log(data.data)
+
+  const tableHeader = `
+      <table> 
+        <tr>
+          <th>Identificador</th>
+          <th>Nome</th>
+          <th>Descrição</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Categoria</th>
+        </tr>
+    `
+
+  let   tableContent = ``
+  const tableFooter  = `</table>`
+
+  data.data.forEach(product => {
+      tableContent += `
+        <tr>
+          <td>${product._id}</td>
+          <td>${product.name}</td>
+          <td>${product.description}</td>
+          <td>${product.price}</td>
+          <td>${product.quantity}</td>
+          <td>${product.category}</td>
+        </tr>
+    `
+  })
+  
+  table.innerHTML = tableHeader + tableContent + tableFooter
+  
+})
+.catch((error) => {
+  console.log(`Error: ${error}`)
+})
+
